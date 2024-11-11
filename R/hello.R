9cc49d22314391e23eeeb2a31599aff0eaa8e06c
sssh-sabi <sabina.shkembi10@gmail.com>
2024-11-11 17:46:27 +0100

    Add files via upload

---
+++ R/hello.R
@@ -0,0 +1,33 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+#' @param n Number of rows in Pascal's Triangle.
+#' @return A list where each element is a row in Pascal's Triangle.
+#' @export
+pascals_triangle <- function(n) {
+  triangle <- list()
+  for (i in 0:(n - 1)) {
+    row <- numeric(i + 1)
+    row[1] <- 1
+    row[i + 1] <- 1
+    if (i > 1) {
+      for (j in 2:i) {
+        row[j] <- triangle[[i]][j - 1] + triangle[[i]][j]
+      }
+    }
+    triangle[[i + 1]] <- row
+  }
+  return(triangle)
+}
