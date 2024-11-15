29bdaa38f493c84c4e986f87b4f5e3e64d7ed6c0
sssh-sabi <sabina.shkembi10@gmail.com>
2024-11-15 17:35:37 +0100

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -19,17 +19,24 @@
 pascals_triangle <- function(n) {
   triangle <- list()
   
-  for (i in 0:(n - 1)) {
+  for (i in 0:(n)) {
     row <- numeric(i + 1)
-    row[0] <- 1
+    row[1] <- 1
     row[i + 1] <- 1
     if (i > 1) {
       for (j in 2:i) {
-        row[j] <- triangle[[i]][j - 1] + triangle[[i]][j]
+        row[j] <- triangle[[i - 1]][j - 1] + triangle[[i - 1]][j]
       }
     }
-    triangle[[i + 1]] <- row
+    triangle[[i]] <- row
   }
   
   return(triangle)
+}
+
+# To print Pascal's Triangle for n = 9
+n <- 9
+triangle <- pascals_triangle(n)
+for (row in triangle) {
+  print(row)
 }
